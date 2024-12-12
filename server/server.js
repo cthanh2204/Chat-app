193612ade344c7c209cc36c5affb84abbe8e7e29
cthanh2204 <84348639+cthanh2204@users.noreply.github.com>
2024-12-12 17:40:42 +0700

    fix bug server

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend"));
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
